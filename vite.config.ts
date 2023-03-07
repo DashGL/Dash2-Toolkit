d1532f04ffb88e33a9ad7bc1d8b8ef35e7132be2
Kion <kion@dashgl.com>
2023-03-07 02:21:40 -0600

    deploy to docs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import path from "path";
 export default defineConfig({
 	base : '/Tool-MML2SaveStateViewer/',
 	resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
+		alias: [
+			{ find: "@", replacement: path.resolve(__dirname, "src") }
+		],
+  	},
+	build: {
+		outDir: 'docs'
+	}
 })
