1994cd4f18055532295636912508369ab2f320ea
Kion <kion@dashgl.com>
2023-03-10 17:10:59 -0600

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import path from "path";
 
 export default defineConfig({
-	base : '/Tool-MML2SaveStateViewer/',
+	// base : '/Tool-MML2SaveStateViewer/',
 	resolve: {
 		alias: [
 			{ find: "@", replacement: path.resolve(__dirname, "src") }
