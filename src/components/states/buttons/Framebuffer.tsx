86d510273518fa5a003f7f8ccda49423ff7b55d8
kion <kion@dashgl.com>
2023-07-11 23:14:22 +0900

    Add framebuffer button

---
+++ src/components/states/buttons/Framebuffer.tsx
@@ -0,0 +1,74 @@
+/*
+
+    Copyright (C) 2023 DashGL - Benjamin Collins
+    This file is part of MML2 StateViewer
+
+    MML2 StateViewer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MML2 StateViewer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MML2 StateViewer. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+const Framebuffer = () => {
+  const handleClick = () => {
+    console.log("click");
+  };
+
+  return (
+    <li onClick={handleClick}>
+      <a
+        data-tooltip-target="frame-tooltip"
+        data-tooltip-placement="left"
+        class="flex items-center p-2 text-gray-500 rounded-lg transition duration-75 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+      >
+        <svg
+          class="w-6 h-6"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 20 18"
+        >
+          <path
+            fill="currentColor"
+            d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"
+          />
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M18 1H2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
+          />
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"
+          />
+        </svg>
+        <span class="sr-only"></span>
+      </a>
+      <div
+        id="frame-tooltip"
+        role="tooltip"
+        class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
+      >
+        View Framebuffer
+        <div class="tooltip-arrow" data-popper-arrow></div>
+      </div>
+    </li>
+  );
+};
+
+export default Framebuffer;
